6a4145d4ae09e518a5ef9adc0b1129a95fedafd5
Chloe Zhang <87879883+NinjaKnows1@users.noreply.github.com>
2025-03-24 19:57:00 -0700

    Delete public directory

--- public/video.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const video = document.getElementById('birthday-video');
-    const card = document.getElementById('birthday-card');
-
-    // When the video ends, show the card
-    video.addEventListener('ended', function() {
-        card.style.display = 'block';
-    });
-});
